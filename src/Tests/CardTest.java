53968f1bbc3359a5dada6134e4af616abde40307
AdamRomyn <adamromyn12345@gmail.com>
2019-03-06 12:06:30 +0200

    Added test suite and also tests for the card class.

---
+++ src/Tests/CardTest.java
@@ -0,0 +1,35 @@
+package Tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import Poker.Card;
+
+public class CardTest {
+	private Card card1;
+	private Card card2;
+	
+	@Before
+	public void before() {
+		System.out.println("Setting up the test case data!");
+		card1 = new Card("AS");
+		card2 = new Card("10S");
+	}
+
+	@Test
+	public void testProperties() {
+		assertTrue(this.card1.getSuit() == 'S');
+		assertTrue(this.card1.getSuit() == this.card2.getSuit());
+	}
+	
+	@Test
+	public void testCompareTo() {
+		assertEquals(this.card1,this.card1);
+		assertEquals(this.card1.compareTo(this.card2),4);
+		assertEquals(this.card2.compareTo(this.card1),-4);
+	}
+
+}
